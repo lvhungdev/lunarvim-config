10287361b3ab8393e7c3522eb6f4f72ad3a4256b
Lê Viết Hùng <lvhu@netcompany.com>
2023-09-07 14:53:15 +0700

    Updated bufferline

--- lua/lvh/options.lua
+++ lua/lvh/options.lua
@@ -20,6 +20,14 @@ lvim.builtin.bufferline.options = {
         }
     },
 }
+lvim.builtin.bufferline.highlights = {
+    buffer_selected = { italic = false },
+    diagnostic_selected = { italic = false },
+    hint_selected = { italic = false },
+    pick_selected = { italic = false },
+    pick_visible = { italic = false },
+    pick = { italic = false },
+}
 
 lvim.builtin.nvimtree.setup = {
     filters = {
